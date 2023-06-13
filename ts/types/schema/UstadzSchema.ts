6258884f3464bb786f9ca8185851dd0fc782ce54
LukmanRambe <lukmanrambe1945@gmail.com>
2023-06-14 06:01:53 +0700

    feat: create ustadz

---
+++ ts/types/schema/UstadzSchema.ts
@@ -0,0 +1,11 @@
+export type CreateUstadzFormValues = {
+  nama_ustadz: string;
+  alamat: string;
+  nomor_wa: string;
+};
+
+export type EditUstadzFormValues = {
+  nama_ustadz: string;
+  alamat: string;
+  nomor_wa: string;
+};
